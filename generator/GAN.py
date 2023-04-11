4e692681e95dfa3c962387c01459573aecad5fc8
Rohit John <rohitpauljohn@gmail.com>
2023-04-11 23:46:28 +0100

    added imports and useful functions

---
+++ generator/GAN.py
@@ -0,0 +1,80 @@
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import torch, time, os, pickle
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import os, gzip, torch
+import torch.nn as nn
+import numpy as np
+import scipy.misc
+import imageio
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+import argparse, os, torch, time, pickle
+
+def print_network(net):
+    num_params = 0
+    for param in net.parameters():
+        num_params += param.numel()
+    print(net)
+    print('Total number of parameters: %d' % num_params)
+
+def save_images(images, size, image_path):
+    return imsave(images, size, image_path)
+
+def imsave(images, size, path):
+    image = np.squeeze(merge(images, size))
+    image = (image * 255).astype(np.uint8)
+    return imageio.imwrite(path, image)
+
+def merge(images, size):
+    h, w = images.shape[1], images.shape[2]
+    if (images.shape[3] in (3,4)):
+        c = images.shape[3]
+        img = np.zeros((h * size[0], w * size[1], c))
+        for idx, image in enumerate(images):
+            i = idx % size[1]
+            j = idx // size[1]
+            img[j * h:j * h + h, i * w:i * w + w, :] = image
+        return img
+    elif images.shape[3]==1:
+        img = np.zeros((h * size[0], w * size[1]))
+        for idx, image in enumerate(images):
+            i = idx % size[1]
+            j = idx // size[1]
+            img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
+        return img
+    else:
+        raise ValueError('in merge(images,size) images parameter ''must have dimensions: HxW or HxWx3 or HxWx4')
+
+
+def generate_animation(path, num):
+    images = []
+    for e in range(num):
+        img_name = path + '_epoch%03d' % (e+1) + '.png'
+        images.append(imageio.imread(img_name))
+    imageio.mimsave(path + '_generate_animation.gif', images, fps=5)
+
+
+def loss_plot(hist, path = 'Train_hist.png', model_name = ''):
+    x = range(len(hist['G_loss']))
+
+    y2 = hist['G_loss']
+
+    plt.plot(x, y2, label='G_loss')
+
+    plt.xlabel('Iter')
+    plt.ylabel('Loss')
+
+    plt.legend(loc=4)
+    plt.grid(True)
+    plt.tight_layout()
+
+    path = os.path.join(path, model_name + '_loss.png')
+
+    plt.savefig(path)
+
+    plt.close()
+
+ 
